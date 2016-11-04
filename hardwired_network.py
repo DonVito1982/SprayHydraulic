90735209aeb964234efdca9b96a79446d78beff0
Víctor Zambrano inelectra <victor.zambrano@inelectra.com>
2016-11-03 20:56:14 -0400

    Solve 3 reservoir problem

--- hardwired_network.py
+++ hardwired_network.py
@@ -4,7 +4,7 @@ import numpy
 
 network_nodes = []
 
-elevations = [100, 85, 60, 85]
+elevations = [100, 90, 65, 85]
 
 for x in range(4):
     cur_node = nodes.Node()
@@ -12,8 +12,8 @@ for x in range(4):
     cur_node.set_elevation(elevations[x], 'm')
     network_nodes.append(cur_node)
 
-lengths = [100, 150, 150]
-diameters = [12, 10, 10]
+lengths = [1500, 1500, 1500]
+diameters = [7.981, 6.065, 6.065]
 network_pipes = []
 for x in range(3):
     cur_pipe = pipes.Pipe()
@@ -22,7 +22,13 @@ for x in range(3):
     cur_pipe.set_c_coefficient(100)
     network_pipes.append(cur_pipe)
 
-vol_flows = numpy.array([1, 2, 3])
+vol_flows = numpy.array([10, -10, 20])
+
+flow_vector = numpy.zeros([4, 1])
+for cont in range(3):
+    flow_vector[cont][0] = vol_flows[cont]
+
+flow_vector[3][0] = 1
 
 
 def k_factor(pipe):
@@ -36,8 +42,8 @@ def k_factor(pipe):
     length = pipe.get_length('ft')
     flow = pipe.get_vol_flow('gpm')
     c = pipe.get_c_coefficient()
-    diam_in = pipe.get_inner_diam('in')
-    factor = (4.52 * length * abs(flow) ** 0.85) / (c ** 1.85 * diam_in ** 4.87)
+    diam = pipe.get_inner_diam('in')
+    factor = (4.52 * length * abs(flow) ** 0.852) / (c ** 1.852 * diam ** 4.87)
     return factor
 
 
@@ -46,32 +52,61 @@ def flow_jacob(pipe):
     flow = pipe.get_vol_flow('gpm')
     c = pipe.get_c_coefficient()
     diam = pipe.get_inner_diam('in')
-    factor = (8.362 * abs(flow) ** 0.85 * length) / (c ** 1.85 * diam ** 4.87)
+    factor = (8.37104 * abs(flow) ** 0.852 * length) / \
+             (c ** 1.852 * diam ** 4.87)
     return factor
 
+jacobian = numpy.zeros([3, 3])
 
-for x in range(3):
-    network_pipes[x].set_vol_flow(vol_flows[x], 'gpm')
 
-f_matrix = numpy.array([[1, -1, -1, 0]])
-n1_energy = network_nodes[0].get_energy('psi')
-n2_energy = network_nodes[1].get_energy('psi')
-n3_energy = network_nodes[2].get_energy('psi')
-first_eq = [-k_factor(network_pipes[0]), -k_factor(network_pipes[1]), 0,
-            n1_energy - n2_energy]
-second_eq = [-k_factor(network_pipes[0]), 0, -k_factor(network_pipes[2]),
-             n1_energy - n3_energy]
+def f_module(vertical_vector):
+    total = 0
+    for module_counter in range(vertical_vector.shape[0]):
+        total += vertical_vector[module_counter][0] ** 2
+    return total
 
-f_matrix = numpy.append(f_matrix, [first_eq], axis=0)
-f_matrix = numpy.append(f_matrix, [second_eq], axis=0)
+iterations = 0
+f_results = numpy.array([[2], [2]])
+print f_module(f_results)
 
-# print f_matrix
+while f_module(f_results) > 0.00001:
 
-flow_vector = numpy.array([[1]])
-for x in range(2, -1, -1):
-    flow_vector = numpy.insert(flow_vector, [0],
-                               [[network_pipes[x].get_vol_flow('gpm')]], axis=0)
+    for x in range(3):
+        network_pipes[x].set_vol_flow(flow_vector[x][0], 'gpm')
 
-f_results = numpy.dot(f_matrix, flow_vector)
-# print f_results
+    f_matrix = numpy.array([[1, -1, -1, 0]])
+    n1_energy = network_nodes[0].get_energy('psi')
+    n2_energy = network_nodes[1].get_energy('psi')
+    n3_energy = network_nodes[2].get_energy('psi')
+    first_eq = [-k_factor(network_pipes[0]), -k_factor(network_pipes[1]), 0,
+                n1_energy - n2_energy]
+    second_eq = [-k_factor(network_pipes[0]), 0, -k_factor(network_pipes[2]),
+                 n1_energy - n3_energy]
 
+    f_matrix = numpy.append(f_matrix, [first_eq], axis=0)
+    f_matrix = numpy.append(f_matrix, [second_eq], axis=0)
+
+    f_results = numpy.dot(f_matrix, flow_vector)
+    print "f-results %d" % iterations
+    print f_module(f_results)
+    print
+
+    flow_equation = [1, -1, -1]
+    for x in range(3):
+        jacobian[0][x] = flow_equation[x]
+
+    row_1 = [-flow_jacob(network_pipes[0]), -flow_jacob(network_pipes[1]), 0]
+    row_2 = [-flow_jacob(network_pipes[0]), 0, -flow_jacob(network_pipes[2])]
+
+    for x in range(3):
+        jacobian[1][x] = row_1[x]
+        jacobian[2][x] = row_2[x]
+
+    delta = -numpy.linalg.solve(jacobian, f_results)
+
+    delta = numpy.append(delta, [[0]], axis=0)
+
+    flow_vector = numpy.add(flow_vector, delta)
+    iterations += 1
+
+print flow_vector
